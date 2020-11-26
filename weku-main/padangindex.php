701b88b9812075e1763e1cb9bf31a37ff18b5a0b
amirulrizqi <68941173+amirulrizqi@users.noreply.github.com>
2020-11-26 21:36:16 +0700

    Add files via upload

---
+++ weku-main/padangindex.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<head class="sateayam">Copyright&copy; - Syauqi.All Rights Reserved</head>
+	<meta charset="UTF-8">
+	<title>Cahaya Taubat</title>
+	<link rel="stylesheet" href="stylect.css">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+</head>
+<body>
+	<div class="wrapper">
+			<nav class="navbar">
+				<img class="logo" src="riser.png" >
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Feedback</a></li>
+				</ul>
+			</nav>
+			<div class="center">
+			<h1>Ingin beli Masakan Padang?</h1>
+			<h2> Cahaya Taubat aja</h2>
+			<div class="buttons">
+			<button onclick="window.location.href='index.php'">Sign in</button>
+			<button class="btn2" onclick="window.location.href='signup.php'">Sign Up</button>
+		</div>
+		</div>
+		
+</body>
+</html>
